7b9f428d69955ae6f42a75439e5b8042e21a6464
mzayem <mzayemazam@gmail.com>
2026-01-03 00:59:29 +0500

    charactor model

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { lazy, Suspense } from "react";
 import "./App.css";
+
+const CharacterModel = lazy(() => import("./components/Character"));
+const MainContainer = lazy(() => import("./components/MainContainer"));
 import { LoadingProvider } from "./context/LoadingProvider";
 
-const MainContainer = lazy(() => import("./components/MainContainer"));
-
-function App() {
+const App = () => {
   return (
     <>
       <LoadingProvider>
         <Suspense>
-          <MainContainer />
+          <MainContainer>
+            <Suspense>
+              <CharacterModel />
+            </Suspense>
+          </MainContainer>
         </Suspense>
       </LoadingProvider>
     </>
   );
-}
+};
 
 export default App;
